06d99a19a7efab8d2d0ac42ce17916f0cc6c2d76
Bohdan Andrianov <bogd.an.drianov@gmail.com>
2019-11-03 23:48:40 +0200

    add beer handling

--- app/Http/Controllers/API/BeerController.php
+++ app/Http/Controllers/API/BeerController.php
@@ -17,9 +17,31 @@ class BeerController extends Controller
      *
      * @return \Illuminate\Http\Response
      */
-    public function index()
+    public function index(Request $request)
     {
+        $relationFields = [
+            'typeName' => ['beer_types', 'name', 'type_id'],
+            'manufacturerName' => ['manufacturers', 'name', 'manufacturer_id']
+          ];
 
+        $filters = $request->get('filters', []);
+        $query = Beer::with($this->getRelations());
+
+        $selectArr = ['beers.*'];
+
+        foreach($filters as $filterField => $filterValue) {
+            if(isset($relationFields[$filterField])) {
+                $relationField = $relationFields[$filterField];
+                $selectArr[]= "{$relationField[0]}.{$relationField[1]}";
+                $query
+                    ->leftJoin($relationField[0], "beers.{$relationField[2]}", '=', "{$relationField[0]}.id")
+                    ->where("{$relationField[0]}.{$relationField[1]}", $filterValue);
+            }
+        }
+
+        $query->selectRaw(implode(', ', $selectArr));
+
+        return BeerResource::collection($query->get());
     }
 
     /**
@@ -30,7 +52,16 @@ class BeerController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $requestData = $request->all();
+        $validator = $this->getValidator($requestData);
+
+        if ($validator->fails()) {
+            return $validator->errors()->toJson();
+        }
+
+        $beer = Beer::create($requestData);
+        $beer->load($this->getRelations());
+        return new BeerResource($beer);
     }
 
     /**
@@ -41,7 +72,7 @@ class BeerController extends Controller
      */
     public function show($id)
     {
-        //
+        return new BeerResource(Beer::with($this->getRelations())->find($id));
     }
 
     /**
@@ -53,7 +84,20 @@ class BeerController extends Controller
      */
     public function update(Request $request, $id)
     {
-        //
+        $requestData = $request->all();
+        $validator = $this->getValidator($requestData, 'update');
+
+        if ($validator->fails()) {
+            return $validator->errors()->toJson();
+        }
+
+        $beer = Beer::findOrFail($id);
+        $beer->fill($requestData);
+        $beer->save();
+
+        $beer->load($this->getRelations());
+
+        return new BeerResource($beer);
     }
 
     /**
@@ -64,6 +108,30 @@ class BeerController extends Controller
      */
     public function destroy($id)
     {
-        //
+        $beer = Beer::findOrFail($id);
+
+        return response()->json([
+            'isDeleted' => $beer->delete()
+        ]);
+    }
+
+    public function getValidator(array $fields, string $scenario = 'store')
+    {
+        $rules =  [
+            'name' => 'nullable|max:255|unique:beers,name',
+            'description' => 'nullable|max:65535',
+            'manufacturer_id' => 'nullable|exists:manufacturers,id',
+            'type_id' => 'nullable|exists:beer_types,id',
+        ];
+
+        if($scenario == 'store'){
+            $rules['name'] = 'required|max:255|unique:beers,name';
+        }
+        return Validator::make($fields, $rules);
+    }
+
+    public function getRelations(): array
+    {
+        return ['type:id,name', 'manufacturer:id,name'];
     }
 }
